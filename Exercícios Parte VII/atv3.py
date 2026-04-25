6779c6b17211dba258ce43dc0d5322320072e707
Gabriel Fedossi <gabrielfedossi@gmail.com>
2026-04-25 11:32:41 -0400

    Relembrando lista

--- Exercícios Parte VII/atv3.py
+++ Exercícios Parte VII/atv3.py
@@ -1,3 +1,15 @@
 lista = [0,1,2,3,4,5,6,7,8,9,10]
 
 print(lista[1:10])
+print(lista[8:10])
+print(lista[::2])
+print(lista[1::2])
+inverso = lista.sort(reverse=True)
+print(lista)
+normal = lista.sort()
+print(lista)
+print(len(lista))
+print(sum(lista))
+
+print(sum(lista) / len(lista))
+
