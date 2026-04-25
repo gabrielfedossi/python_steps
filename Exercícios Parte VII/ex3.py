8e484fc3124fbc1bb4c951979d4e0e079da47e07
Gabriel Fedossi <gabrielfedossi@gmail.com>
2026-04-25 09:03:28 -0400

    String

---
+++ Exercícios Parte VII/ex3.py
@@ -0,0 +1,14 @@
+a = input("Insira seu nome: ")
+
+
+b = a.find("Fedossi")
+print(b)
+print("Fedossi" in a)
+c = a.split(" ")
+
+
+print("Seu nome é", a)
+print(c)
+print("Quantos 'a' tem no seu nome:", a.count("a"))
+print("Primeira letra do seu nome:", a[0])
+print(a[1:4])
